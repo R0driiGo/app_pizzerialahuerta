29212a95dc47feca50c147ac0a15126877b57b29
R0driiGo <rodrigobh100321@gmail.com>
2024-05-27 01:41:54 -0500

    first commit

---
+++ app_pizzerialahuerta/android/app/src/main/kotlin/app_pizzerialahuerta/ucv/uedu/pe/app_pizzerialahuerta/MainActivity.kt
@@ -0,0 +1,5 @@
+package app_pizzerialahuerta.ucv.uedu.pe.app_pizzerialahuerta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ app_pizzerialahuerta/lib/utils/rutas.dart
@@ -0,0 +1,5 @@
+export 'package:app_pizzerialahuerta/ui/pedidos/registrar_pedido_pizza.dart';
+export 'package:app_pizzerialahuerta/ui/pedidos/registrar_pedido_hamburguesa.dart';
+export 'package:app_pizzerialahuerta/ui/pedidos/registrar_pedido_chifa.dart';
+export 'package:app_pizzerialahuerta/ui/principal/home.dart';
+export 'package:app_pizzerialahuerta/ui/pedidos/detalle_pedido.dart';
